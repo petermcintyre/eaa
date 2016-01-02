4286de054ec0c24539e6414d5b11017f052d38ef
Chris Leong <walkraft@gmail.com>
2016-01-02 23:23:00 +1100

    Always uses small header

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -25,13 +25,13 @@ var cbpAnimatedHeader = (function() {
 	}
 
 	function scrollPage() {
-		var sy = scrollY();
+		/*var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
 			classie.add( header, 'navbar-shrink' );
 		}
 		else {
 			classie.remove( header, 'navbar-shrink' );
-		}
+		}*/
 		didScroll = false;
 	}
 
