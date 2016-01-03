cbdcd29bbbb2030f6c5b93bf7cd034a0676eddd1
Chris Leong <walkraft@gmail.com>
2016-01-04 03:04:07 +1100

    Added missing files. Links should now work

--- js/donate.js
+++ js/donate.js
@@ -1,3 +1,9 @@
 $("#bank-transfer-link").click(function(){
+    $("#cheque-details").hide();
     $("#bank-transfer-details").toggle();
+});
+
+$("#cheque-link").click(function(){
+    $("#bank-transfer-details").hide();
+    $("#cheque-details").toggle();
 });
