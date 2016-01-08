24b13305a4d2461cbef413fdf92635720642099a
Chris Leong <walkraft@gmail.com>
2016-01-08 17:54:07 +1100

    Removed mobile detection - sadly doesn't work due to depreciation

--- js/eaa.js
+++
@@ -1,8 +0,0 @@
-/*Replace tel: links with callto: so that it works with Skype
-* Source:http://stackoverflow.com/questions/1164004/how-to-mark-up-phone-numbers*/
-if (!jQuery.browser.mobile) {
-    jQuery('body').on('click', 'a[href^="tel:"]', function() {
-        jQuery(this).attr('href',
-            jQuery(this).attr('href').replace(/^tel:/, 'callto:'));
-    });
-}
