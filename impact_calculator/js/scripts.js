bf646aeb919558b7074587ca596c82cfad7d54b3
Chris Leong <walkraft@gmail.com>
2016-01-08 23:14:48 +1100

    Moved impact calculator into its own folder

--- impact_calculator/js/scripts.js
+++ impact_calculator/js/scripts.js
@@ -339,6 +339,7 @@ function writeResults(charity, impacts, elements){
 $(function(){
 
   // Toolbar scroll
+  console.log($('.ic-content').waypoint);
   $('.ic-content').waypoint(function(direction){
     if (direction === 'down') {
       $('.toolbar').addClass('toolbar--scroll');
